dd2c1637b27f11ed7780ab9df9c1e1ea7a2b3f6b
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2025-10-15 11:03:37 +0100

    Update evaluation class and tests
    Adds changes from:
    - https://github.com/alphagov/search-api-v2/pull/558
    - https://github.com/alphagov/search-api-v2/pull/567
    - https://github.com/alphagov/search-api-v2/pull/564
    
    Audit trail:
    evaluation.rb: https://github.com/alphagov/search-api-v2/blob/dc8aa54fab3ea4a376c3136a2a3ff0569e7b799a/app/services/discovery_engine/quality/evaluation.rb
    evaluation_spec.rb: https://github.com/alphagov/search-api-v2/blob/dc8aa54fab3ea4a376c3136a2a3ff0569e7b799a/spec/services/discovery_engine/quality/evaluation_spec.rb
    shared_examples.rb: https://github.com/alphagov/search-api-v2/blob/dc8aa54fab3ea4a376c3136a2a3ff0569e7b799a/spec/services/discovery_engine/quality/shared_examples.rb

--- app/services/discovery_engine/quality/evaluation.rb
+++ app/services/discovery_engine/quality/evaluation.rb
@@ -1,5 +1,8 @@
 module DiscoveryEngine::Quality
   class Evaluation
+    MAX_RETRIES = 5
+    WAIT_TIME = 60
+
     attr_reader :sample_set
 
     def initialize(sample_set)
@@ -7,19 +10,23 @@ module DiscoveryEngine::Quality
     end
 
     def quality_metrics
-      api_response.quality_metrics.to_h
+      fetched_evaluation.quality_metrics.to_h
     end
 
-    # evaluation_name and api_response.name are equivalent, but calling api_response
+    # evaluation_name and fetched_evaluation.name are equivalent, but calling fetched_evaluation
     # ensures that we have fetched an evaluation before we ask for list_results.
     def list_evaluation_results
-      ListEvaluationResults.new(api_response.name, sample_set.display_name).formatted_json
+      ListEvaluationResults.new(
+        fetched_evaluation.name,
+        sample_set.display_name,
+        serving_config,
+      ).formatted_json
     end
 
     def formatted_create_time
-      raise "Error: cannot provide create time of an evaluation unless one exists" if @api_response.blank?
+      raise "Error: cannot provide create time of an evaluation unless one exists" if @fetched_evaluation.blank?
 
-      google_time_stamp = @api_response.create_time
+      google_time_stamp = @fetched_evaluation.create_time
       if google_time_stamp
         data = { nanos: google_time_stamp.nanos, seconds: google_time_stamp.seconds }
         Google::Protobuf::Timestamp.new(data).to_time.strftime("%Y-%m-%d %H:%M:%S")
@@ -30,20 +37,56 @@ module DiscoveryEngine::Quality
 
     attr_reader :evaluation_name
 
-    def api_response
-      @api_response ||= fetch_api_response
+    def evaluation_states
+      {
+        active: %i[PENDING RUNNING],
+        finished: %i[SUCCEEDED FAILED],
+      }
     end
 
-    def fetch_api_response
+    def serving_config
+      raise "Error: cannot provide serving config of an evaluation unless one exists" if @fetched_evaluation.blank?
+
+      @fetched_evaluation.evaluation_spec.search_request.serving_config
+    end
+
+    def fetched_evaluation
+      @fetched_evaluation ||= fetch_evaluation
+    end
+
+    def fetch_evaluation
+      wait_for_active_evaluations_to_finish
       create_evaluation
-      get_evaluation_with_wait
+      get_evaluation_with_wait(evaluation_name)
+    end
+
+    def wait_for_active_evaluations_to_finish
+      return if active_evaluations.blank?
+
+      active_evaluations.each do |e|
+        Rails.logger.info("Waiting for #{e.name} to finish")
+        attempts = 0
+
+        while (e = get_evaluation(e.name))
+          break if evaluation_states[:finished].include?(e.state)
+          break if (attempts += 1) > MAX_RETRIES
+
+          Kernel.sleep(WAIT_TIME)
+        end
+      end
+    end
+
+    def active_evaluations
+      @active_evaluations ||=
+        evaluation_service
+          .list_evaluations(parent:)
+          .select { |e| evaluation_states[:active].include?(e.state) }
     end
 
     def create_evaluation
-      operation = DiscoveryEngine::Clients
-        .evaluation_service
+      operation = evaluation_service
         .create_evaluation(
-          parent: Rails.application.config.discovery_engine_default_location_name,
+          parent:,
           evaluation: {
             evaluation_spec: {
               query_set_spec: {
@@ -67,19 +110,27 @@ module DiscoveryEngine::Quality
       raise e
     end
 
-    def get_evaluation_with_wait
+    def get_evaluation_with_wait(name)
       Rails.logger.info("Fetching evaluations...")
 
-      while (e = get_evaluation)
+      while (e = get_evaluation(name))
         return e if e.state == :SUCCEEDED
 
         Rails.logger.info("Still waiting for evaluation to complete...")
-        Kernel.sleep(10)
+        Kernel.sleep(WAIT_TIME)
       end
     end
 
-    def get_evaluation
-      DiscoveryEngine::Clients.evaluation_service.get_evaluation(name: evaluation_name)
+    def get_evaluation(name)
+      evaluation_service.get_evaluation(name:)
+    end
+
+    def evaluation_service
+      @evaluation_service ||= DiscoveryEngine::Clients.evaluation_service
+    end
+
+    def parent
+      @parent ||= Rails.application.config.discovery_engine_default_location_name
     end
   end
 end
